ade4406a56d6fdc2818bd0f9305ad205962e659a
jwCheng28 <jwCheng28@users.noreply.github.com>
2020-07-04 12:02:55 -0700

    Added more comments and modified output plots

--- logistic_reg.py
+++ logistic_reg.py
@@ -19,18 +19,26 @@ def plotData(X, y, theta, history):
     fig = plt.figure(1)
     pos = y == 1
     neg = y == 0
-    plt.plot(X[neg, 1], X[neg, 2], 'ro')
-    plt.plot(X[pos, 1], X[pos, 2], 'bo')
+    # Plot different cluster with different color
+    plt.plot(X[neg, 1], X[neg, 2], 'ro', label = "Negative Class")
+    plt.plot(X[pos, 1], X[pos, 2], 'bo', label = "Positive Class")
 
+    # y = mx + b => X_2 = -(theta_0 + theta_1 * X_1)/theta_2
     slope = -(theta[1] / theta[2])
-    intercept = -(theta[0] / theta[2])
+    b = -(theta[0] / theta[2])
     ax = plt.gca()
     x_vals = np.array(ax.get_xlim())
-    y_vals = intercept + (slope * x_vals)
-    plt.plot(x_vals, y_vals, c="k");
+    y_vals = b + (slope * x_vals)
+    plt.plot(x_vals, y_vals, c="k", label = "Hypothesis Function");
+    plt.xlabel("Random Data X_1")
+    plt.ylabel("Random Data X_2")
+    plt.legend()
 
+    # History of Cost
     fig = plt.figure(2)
     plt.plot([i for i in range(len(history))], history, '-')
+    plt.xlabel("Iterations")
+    plt.ylabel("Cost")
     plt.show()
 
 # Sigmoid Function
