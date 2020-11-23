c13af1abaa4cac0174a2c66c2f9d90f8672e94db
jwCheng28 <jwCheng28@users.noreply.github.com>
2020-11-22 18:30:53 -0800

    Basic setup of CART Decision Tree

---
+++ algorithms/TreeModel/decision.hpp
@@ -0,0 +1,29 @@
+#ifndef _DECISION_H
+#define _DECISION_H
+
+// CART DECISION TREE
+// Currently only takes in numerical features and labels
+
+#include <vector>
+#include <tuple>
+
+namespace Tree {
+
+class Test;
+class Leaf;
+class Decision;
+
+class DecisionTree{
+    private:
+        std::vector<std::vector<float>> mat;
+    public:
+        DecisionTree(std::vector<std::vector<float>> dt) : mat(dt) {}
+        ~DecisionTree() {}
+        std::vector<std::vector<std::vector<float>>> splitTree(Test test);
+        float gini(std::vector<std::vector<float>> data);
+        float gain(std::vector<std::vector<float>> left, std::vector<std::vector<float>> right, float entropy);
+        std::tuple<float, Test> best_split(std::vector<std::vector<float>> data);
+        DecisionTree constructTree();
+};
+
+} // Tree Namespace
