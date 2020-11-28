ea257bdbf915c07270684bc6834846a4b8e6f8ce
jwCheng28 <jwCheng28@users.noreply.github.com>
2020-11-27 21:06:03 -0800

    Updated implementation after removing X and y var from class

--- algorithms/LinearModel/linreg.cpp
+++ algorithms/LinearModel/linreg.cpp
@@ -3,23 +3,23 @@
 
 namespace Linear {
 
-float LinearRegression::costFunction(){
+float LinearRegression::costFunction(Eigen::MatrixXd X, Eigen::VectorXd y){
     Eigen::VectorXd h = X * theta;
     // Mean Square Error
     return (1.0 / (2 * m)) * (h - y).array().pow(2).sum();
 }
 
-void LinearRegression::gradientDescent(float alpha, int epochs, std::vector<float>& hist, bool cost){
+void LinearRegression::gradientDescent(const Eigen::MatrixXd& X, const Eigen::VectorXd& y, float alpha, int epochs, std::vector<float>& hist, bool cost){
     for (int i = 0; i < epochs; i++) {
         // th -= (a/m) * (X.dot(th) - y).dot(X)
         theta -= (alpha / m) * (((X * theta) - y).transpose() * X).transpose();
-        hist.push_back(costFunction());
+        hist.push_back(costFunction(X, y));
         if (cost)
             std::cout << "Epoch: " << i+1 << ", Cost: " << hist[i] << std::endl;
     }
 }
 
-void LinearRegression::normalEquation(){
+void LinearRegression::normalEquation(const Eigen::MatrixXd& X, const Eigen::VectorXd& y){
     Eigen::MatrixXd Xt = X.transpose();
     Eigen::MatrixXd I = (Xt * X).inverse();
     theta = (I * Xt) * y;
--- algorithms/LinearModel/logreg.cpp
+++ algorithms/LinearModel/logreg.cpp
@@ -7,17 +7,17 @@ Eigen::MatrixXd LogisticRegression::sigmoid(Eigen::MatrixXd mat){
     return 1.0 / (1 + (-1 * mat.array()).exp());
 }
 
-float LogisticRegression::costFunction(){
+float LogisticRegression::costFunction(Eigen::MatrixXd X, Eigen::VectorXd y){
     Eigen::VectorXd h = sigmoid(X * theta);
     return (-1.0 / m) * 
         ((y.array()) * h.array().log() + (1 - y.array()) * (1 - h.array()).log()).sum();
 }
 
-void LogisticRegression::gradientDescent(float alpha, int epochs, std::vector<float>& hist, bool cost){
+void LogisticRegression::gradientDescent(const Eigen::MatrixXd& X, const Eigen::VectorXd& y, float alpha, int epochs, std::vector<float>& hist, bool cost){
     for (int i = 0; i < epochs; i++) {
         // th -= (a/m) * (sig(X.dot(th)) - y).dot(X)
         theta -= (alpha / m) * ((sigmoid(X * theta) - y).transpose() * X).transpose();
-        hist.push_back(costFunction());
+        hist.push_back(costFunction(X, y));
         if (cost)
             std::cout << "Epoch: " << i+1 << ", Cost: " << hist[i] << std::endl;
     }
