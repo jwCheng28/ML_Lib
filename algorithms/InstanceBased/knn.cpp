280aa7f3ce0154ad11ef7ec9e5721388e8b16c34
jwCheng28 <chengjw68@gmail.com>
2020-12-24 13:56:44 -0800

    Added setter function, remove unnecesary lines from predict function

--- algorithms/InstanceBased/knn.cpp
+++ algorithms/InstanceBased/knn.cpp
@@ -5,7 +5,16 @@
 
 namespace Instance {
 
+void setFeature(int k, int p) {
+    k_neighbor = k;
+    minkowski_p = p;
+}
+
 float KNN::minkowski(Eigen::VectorXd x1, Eigen::VectorXd x2){
+    /*
+    output:
+        float -> returns minkowski distance of the 2 vector
+     */
     if (x1.size() != x2.size())
         throw std::invalid_argument("Vector Length Does Not Match");
     return (x1 - x2).array().abs().pow(minkowski_p).sum();
@@ -14,11 +23,11 @@ float KNN::minkowski(Eigen::VectorXd x1, Eigen::VectorXd x2){
 std::vector<int> KNN::predict(Eigen::MatrixXd& train_X, Eigen::VectorXd& train_y, Eigen::MatrixXd& test){
     /*
     param:
-        Eigen::MatrixXd& -> training data X (features)
-        Eigen::VectorXd& -> training data y (labels)
-        Eigen::MatrixXd& -> testing data X (features)
+        Eigen::MatrixXd& train_X -> training data X (features)
+        Eigen::VectorXd& train_y -> training data y (labels)
+        Eigen::MatrixXd& test -> testing data X (features)
     output:
-        std::vector<int> -> vector of test data prediction
+        std::vector<int> result -> vector of test data prediction
     */
     std::vector<int> result;
     for (int r = 0; r < test.rows(); r++) {
@@ -31,15 +40,13 @@ std::vector<int> KNN::predict(Eigen::MatrixXd& train_X, Eigen::VectorXd& train_y
         std::sort(pts_dist.begin(), pts_dist.end());
         std::vector<std::tuple<float, int>> _knn(pts_dist.begin(), pts_dist.begin() + k_neighbor);
         
-        // Decide Best Label based on Nearest Neighbors (Mode Occurance)
+        // Decide Best Label based on Nearest Neighbors (Highest Freq)
         std::unordered_map<int, int> label_count;
         int max_count = INT_MIN, best_label, index;
         float mks_dist;
         for (const auto& nn: _knn) {
             std::tie(mks_dist, index) = nn;
             int label = train_y[index];
-            if (!label_count.count(label))
-                label_count[label] = 0;
             if (++label_count[label] > max_count) {
                 max_count = label_count[label];
                 best_label = label;
