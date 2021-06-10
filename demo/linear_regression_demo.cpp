1bbf9cd5c6f797512a7d30d0cc74bc96d763f31a
jwCheng28 <jwCheng28@users.noreply.github.com>
2021-06-10 15:48:37 -0700

    Updated demo for linear regression

--- demo/linear_regression_demo.cpp
+++ demo/linear_regression_demo.cpp
@@ -32,45 +32,63 @@ void display(vector<vector<string>>& data) {
     }
 }
 
-int main() {
-
-    // CSV_READER *csv_file = new CSV_READER("data/insurance.csv");
-    CSV_READER *csv_file = new CSV_READER("data/test1.csv");
-    vector<vector<string>> data = csv_file->loadAll(true);
-    int rows = data.size(), cols = data[0].size();
-
-    unordered_map<int, unordered_map<string, string>> encoding = {
-            {1, {{"female", "0"}, {"male", "1"}}},
-            {4, {{"no", "0"}, {"yes", "1"}}},
-    };
-    // encode_data(data, encoding, 1);
-
-    MatrixXd dataM = csv_file->csvToMat(true, data);
-    MatrixXd dataMat = Tensor::addOne(dataM);
-    vector<MatrixXd> train_test_data = Tensor::split(dataMat, 1, rows*0.8);
+void GDtest(MatrixXd data, int rows, int cols) {
+    data = Tensor::normalize(data);
+    MatrixXd dataMat = Tensor::addOne(data);
+    vector<MatrixXd> train_test_data = Tensor::split(dataMat, 1, rows*0.7);
     vector<MatrixXd> trainXY = Tensor::split(train_test_data[0], 0, cols);
     vector<MatrixXd> testXY = Tensor::split(train_test_data[1], 0, cols);
-    
-//    cout << "trainX: \n";
-//    cout << trainXY[0] << '\n';
-//    cout << "trainY: \n";
-//    cout << trainXY[1] << '\n';
 
-
-    cout << "items: " << trainXY[0].rows() << "  | features: " << trainXY[0].cols() << '\n';
     Linear::LinearRegression linreg(trainXY[0].rows(), trainXY[0].cols());
+    cout << "Linear Regression - Gradient Descent\n";
+    vector<float> costHistory;
+    linreg.gradientDescent(
+            trainXY[0], trainXY[1], 
+            1.3, 16, 
+            costHistory, true);
+    cout << "Result Thetas: \n";
+    cout << linreg.getTheta() << "\n\n";
+    cout << "Test Accuracy: " << Tensor::similarity(testXY[0] * linreg.getTheta(), testXY[1]) * 100 << "%\n";
+}
 
-//    vector<float> costHistory;
-//    linreg.gradientDescent(trainXY[0], trainXY[1], 0.00012, 10, costHistory, true);
-//    cout << "Thetas: \n";
-//    cout << linreg.getTheta() << '\n';
+void NEtest(MatrixXd data, int rows, int cols) {
+    MatrixXd dataMat = Tensor::addOne(data);
+    vector<MatrixXd> train_test_data = Tensor::split(dataMat, 1, rows*0.7);
+    vector<MatrixXd> trainXY = Tensor::split(train_test_data[0], 0, cols);
+    vector<MatrixXd> testXY = Tensor::split(train_test_data[1], 0, cols);
 
+    Linear::LinearRegression linreg(trainXY[0].rows(), trainXY[0].cols());
+    cout << "Linear Regression - Normal Equation\n";
     linreg.normalEquation(trainXY[0], trainXY[1]);
-    cout << "Cost: " << linreg.costFunction(trainXY[0], trainXY[1]) << '\n';
     cout << "Thetas: \n";
-    cout << linreg.getTheta() << '\n';
+    cout << linreg.getTheta() << "\n\n";
+    cout << "Test Accuracy: " << Tensor::similarity(testXY[0] * linreg.getTheta(), testXY[1]) * 100 << "%\n";
+}
 
-    cout << "Accuracy: " << Tensor::similarity(testXY[0] * linreg.getTheta(), testXY[1]) * 100 << '\n';
+void test1() {
+    CSV_READER *csv_file = new CSV_READER("data/test1.csv");
+    cout << "Test Data Loaded\n\n";
+    cout << "Data Equation y = 2x\n";
+    cout << "Theoretical Theta should be t1=0, t2=2\n\n";
 
+    vector<vector<string>> data = csv_file->loadAll(true);
+    int rows = data.size(), cols = data[0].size();
+    MatrixXd dataM = csv_file->csvToMat(true, data);
+    NEtest(dataM, rows, cols);
+}
+
+// TO DO
+void test2() {
+    // CSV_READER *csv_file = new CSV_READER("data/insurance.csv");
+//    unordered_map<int, unordered_map<string, string>> encoding = {
+//            {1, {{"female", "0"}, {"male", "1"}}},
+//            {4, {{"no", "0"}, {"yes", "1"}}},
+//    };
+    // encode_data(data, encoding, 1);
+    return;
+}
+
+int main() {
+    test1();
     return 0;
 }
